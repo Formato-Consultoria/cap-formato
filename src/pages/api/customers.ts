74a27013c4fbe64cb831c4cbef2604cec0a2bcae
root <root@DESKTOP-AN69NUE>
2023-04-16 21:22:31 -0300

    xml format data api/customers

--- src/pages/api/customers.ts
+++ src/pages/api/customers.ts
@@ -1,11 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { getAllCustomerDatas } from "@/controllers/cap-formato.controller";
+import { Builder } from "xml2js";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const datas = await getAllCustomerDatas();
+  const { format } = req.query;
+
+  const datas = await getAllCustomerDatas();
+
+  if (format === "xml") {
+    const builder = new Builder({ rootName: "customers" });
+    const xml = builder.buildObject(datas);
+    res.setHeader("Content-Type", "application/xml");
+    res.status(200).send(xml);
+  } else {
     res.status(200).json(datas);
+  }
 }
